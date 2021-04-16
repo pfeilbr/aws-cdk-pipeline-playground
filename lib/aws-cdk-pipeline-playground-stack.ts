c7afe316b752be1f136880de1e067e1e8edf16a5
Brian Pfeil <brian.pfeil@gmail.com>
2021-04-16 09:40:45 -0400

    added cdk-watchful for CW alarming (email) and dashboard

--- lib/aws-cdk-pipeline-playground-stack.ts
+++ lib/aws-cdk-pipeline-playground-stack.ts
@@ -3,6 +3,8 @@ import { ApiGatewayToLambda } from "@aws-solutions-constructs/aws-apigateway-lam
 import * as defaults from "@aws-solutions-constructs/core";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as api from "@aws-cdk/aws-apigateway";
+//import * as sns from "@aws-cdk/aws-sns";
+import { Watchful } from "cdk-watchful";
 export class AwsCdkPipelinePlaygroundStack extends cdk.Stack {
   public readonly urlOutput: cdk.CfnOutput;
 
@@ -29,6 +31,17 @@ export class AwsCdkPipelinePlaygroundStack extends cdk.Stack {
         },
       }
     );
+
+    // const alarmSns = sns.Topic.fromTopicArn(
+    //   this,
+    //   "AlarmTopic",
+    //   "arn:aws:sns:us-east-1:529276214230:NotifyMe"
+    // );
+    const watchful = new Watchful(this, "watchful", {
+      alarmEmail: "brian.pfeil@gmail.com",
+    });
+
+    watchful.watchScope(apiGatewayToLambda);
 
     this.urlOutput = new cdk.CfnOutput(this, "Url", {
       value: apiGatewayToLambda.apiGateway.url,
